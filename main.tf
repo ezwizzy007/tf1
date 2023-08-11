adb06fdb98c0a44ec21aecf9d78cb93d68241bbb
darrendu <darren25@gmail.com>
2023-08-11 17:31:02 -0400

    changed main sensitive to true

--- main.tf
+++ main.tf
@@ -25,10 +25,10 @@ output "acr_login_server" {
 
 output "acr_admin_username" {
   value     = azurerm_container_registry.acr.admin_username
-  sensitive = false
+  sensitive = true
 }
 
 output "acr_admin_password" {
   value     = azurerm_container_registry.acr.admin_password
-  sensitive = false
+  sensitive = true
 }
